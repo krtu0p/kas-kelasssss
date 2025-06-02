156a5e8a6be3dd43de90cb97b6e969ca0d979473
krtu0p <adiaksab99@gmail.com>
2025-06-02 22:15:32 +0800

    nambah delete minggu we

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('/update', [DashboardController::class, 'update'])->name('pembayaran.update');
     Route::post('/tambah-minggu', [DashboardController::class, 'tambahMinggu'])->name('pembayaran.tambah_minggu');
+    Route::post('/hapus-minggu', [DashboardController::class, 'hapusMinggu'])->name('pembayaran.hapus_minggu');
 });
 
 // Login dan logout untuk bendahara
