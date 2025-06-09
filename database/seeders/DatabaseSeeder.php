f76a2356d8f7bc7d3e8992661482fb7835cd0ba4
krtu0p <adiaksab99@gmail.com>
2025-06-10 03:21:57 +0800

    new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,25 +5,22 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;      // <== Pindahin ke sini
+use Illuminate\Support\Facades\Hash;    // <== Pindahin ke sini
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+    public function run(): void
+    {
+        $this->call(SiswaSeeder::class);
 
-public function run(): void
-{
-    $this->call(SiswaSeeder::class);
-
-    DB::table('users')->insert([
-        'name' => 'Test User',
-        'email' => 'test@example.com',
-        'password' => Hash::make('password'),
-    ]);
-}
-
-
+        DB::table('users')->insert([
+            'name' => 'Test User',
+            'email' => 'admin2@gmail.com',
+            'password' => Hash::make('12345'),
+        ]);
+    }
 }
